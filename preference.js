3b1cc9a18969dbcfbbec29b6a6dc795aa0c8b35a
Akashiba404 <100006506+Akashiba404@users.noreply.github.com>
2022-02-27 14:56:10 +0900

    Add new code

---
+++ preference.js
@@ -0,0 +1,14 @@
+var main_iframe = getElementById("key_setting_button");
+
+
+document.getElementById("key_setting_button").onclick() = function() {
+    document.main_ifarme.src = "setting_page/key_setting.html";
+};
+
+document.getElementById("button_setting_button").onclick() = function() {
+    document.main_ifarme.src = "setting_page/button_setting.html";
+};
+
+document.getElementById("volume_setting_button").onclick() = function() {
+    document.main_ifarme.src = "setting_page/volume_setting.html";
+};
